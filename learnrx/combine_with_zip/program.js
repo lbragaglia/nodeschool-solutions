bf5dd55749ab6d9ef6f02d9f3a162c5208d7a351
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2016-05-24 00:12:13 +0200

    Add more learnrx solutions

---
+++ learnrx/combine_with_zip/program.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (videos, bookmarks) {
+    return Array.zip(videos, bookmarks, function (video, bookmark) {
+        return {
+            videoId: video.id,
+            bookmarkId: bookmark.id
+        }
+    });
+}
---
+++ learnrx/zipping_arrays/program.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (videos, bookmarks) {
+    var counter,
+        videoIdAndBookmarkIdPairs = [];
+
+    for (var counter = 0; counter < Math.min(videos.length, bookmarks.length); counter++) {
+        videoIdAndBookmarkIdPairs.push({
+            videoId: videos[counter].id,
+            bookmarkId: bookmarks[counter].id
+        })
+    }
+
+    return videoIdAndBookmarkIdPairs;
+}
