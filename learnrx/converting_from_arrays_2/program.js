dc8005955be14e97d84a70cea0aa3f4ab8ce5219
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2016-05-28 22:45:01 +0200

    Add more learnrx solutions

---
+++ learnrx/converting_from_arrays_2/program.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function (lists, videos, boxarts, bookmarks) {
+    return lists.map(function (list) {
+        return {
+            name: list.name,
+            videos: videos.filter(function (video) {
+                return video.listId === list.id
+            }).map(function (video) {
+                return Array.zip(boxarts.filter(function (boxart) {
+                    return boxart.videoId === video.id
+                }).reduce(function (smallest, current) {
+                    return current.width * current.height < smallest.width * smallest.height ? current : smallest
+                }), bookmarks.filter(function (bookmark) {
+                    return bookmark.videoId === video.id
+                }), function (boxart, bookmark) {
+                    return {
+                        id: video.id,
+                        title: video.title,
+                        time: bookmark.time,
+                        boxart: boxart.url
+                    }
+                })
+            })
+        }
+    })
+}
